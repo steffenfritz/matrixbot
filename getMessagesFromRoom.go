9db90d52db2b2b727c1929a72180b96a1787185b
Steffen Fritz <steffen@fritz.wtf>
2020-03-03 16:23:13 +0100

    working copy

---
+++ getMessagesFromRoom.go
@@ -0,0 +1,3 @@
+package matrixbot
+
+func GetInitialEvent() {}
--- joinroom.go
+++ joinroom.go
@@ -1,4 +1,4 @@
 package matrixbot
 
 // JoinRoom lets your bot join a room
-func JoinRoom(server stringroomID string, token string) {}
+func JoinRoom(server string, roomID string, token string) {}
