5115b65d3d299be320cf6bc230aaba13c8dbdaa8
steffenfritz <steffen@fritz.wtf>
2020-03-22 17:24:04 +0100

    from fatty

--- config.go
+++ config.go
@@ -9,9 +9,10 @@ type Config struct {
 	Server   string
 	User     string
 	Password string
-	Roomname string
+	RoomID   string
 }
 
+// ReadConfig reads the configuration of a toml file into a Config struct
 func ReadConfig(confFile string) Config {
 	var config Config
 	if _, err := toml.DecodeFile(confFile, &config); err != nil {
@@ -19,4 +20,14 @@ func ReadConfig(confFile string) Config {
 	}
 
 	return config
+}
+
+// ConfigValid checks if the config has at least a server name
+func ConfigValid(config Config) bool {
+
+	if len(config.Server) == 0 {
+		return false
+	}
+
+	return true
 }
